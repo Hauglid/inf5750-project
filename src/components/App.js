47471dce981174931d79c5eb9286a1ac7b225a4c
Hauglid <karltha@ifi.uio.no>
2016-11-01 20:14:51 +0100

    removed unneccessary lines

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { saveOrganisationUnit, loadOrganisationUnits, deleteOrganisationUnit } from '../api';
-import List from './List';
-import Form from './Form';
+
 import Map from './Map';
+import Search from './Search';
 
 export default class App extends Component {
     render() {
         return(
-            <Map />
+            <div>
+                <Map />
+                <Search/>
+            </div>
+
         );
     }
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,3 +1,11 @@
 /**
  * Created by hauglid on 01.11.16.
  */
+
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+    render(){
+        return <h2> karli stuff </h2>
+    }
+}
