061e08c95c22b9f11243c0c627efa47cf1d92d79
Hauglid <karltha@ifi.uio.no>
2016-11-06 16:47:29 +0100

    changed to darktheme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 import Counter from './Components/Counter';
 import Header from "./Components/Header";
 
@@ -7,7 +10,7 @@ import Header from "./Components/Header";
 export default class App extends React.Component {
     render() {
         return (
-            <MuiThemeProvider>
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
                 <div>
                     <Header/>
                     <Counter/>
