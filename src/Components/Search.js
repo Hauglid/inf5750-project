f9ec3f75ab0f7acecc77e01ff6050e9eee5e9330
Hauglid <karltha@ifi.uio.no>
2016-11-06 17:49:35 +0100

    added autocomplete to search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,27 +1,69 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
+import AutoComplete from 'material-ui/AutoComplete';
+import {Toolbar, ToolbarGroup, ToolbarSeparator} from 'material-ui/Toolbar';
+import JSONP from 'jsonp';
 
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+const googleAutoSuggestURL = `
+  //suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=`;
+
 
 export default class ToolbarExamplesSimple extends React.Component {
 
     constructor(props) {
         super(props);
+        this.onUpdateInput = this.onUpdateInput.bind(this);
         this.state = {
-            value: 3,
-        };
+            dataSource: [],
+            inputValue: ''
+        }
     }
 
+    onUpdateInput(inputValue) {
+        const self = this;
+        this.setState({
+            inputValue: inputValue
+        }, function () {
+            self.performSearch();
+        });
+    }
+
+    performSearch() {
+        const
+            self = this,
+            url = googleAutoSuggestURL + this.state.inputValue;
+
+        if (this.state.inputValue !== '') {
+            JSONP(url, function (error, data) {
+                let searchResults, retrievedSearchTerms;
+
+                if (error) return error;
+
+                searchResults = data[1];
+
+                retrievedSearchTerms = searchResults.map(function (result) {
+                    return result[0];
+                });
+
+                self.setState({
+                    dataSource: retrievedSearchTerms
+                });
+            });
+        }
+    }
 
     render() {
         return (
             <Toolbar>
-                <TextField fullWidth="true" hintText="Enter search query here"/>
-
+                <AutoComplete
+                    hintText="Search"
+                    dataSource={this.state.dataSource}
+                    onUpdateInput={this.onUpdateInput}
+                    fullWidth={true}
+                />
                 <ToolbarGroup lastChild={true}>
                     <ToolbarSeparator />
-                    <RaisedButton label="Search" primary={true}/>
+                    <RaisedButton label="Search" primary={true} on/>
 
                 </ToolbarGroup>
             </Toolbar>
