184b7b2c5d1cec1bd53e2ffc43b1346a8bacd888
Hauglid <karltha@ifi.uio.no>
2016-11-07 15:49:55 +0100

    added orgunitinfo to body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -28,6 +28,8 @@ export default class Body extends React.Component {
         return (
             <div style={{width:600, height:300}}>
                 <Map center={location} markers={markers}/>
+                <OrgUnitInfo />
+
             </div>
 
         )
