097086f9395727dc56f99314b3262fb6d2576d50
Jostein Hellerud <jostein1993@hotmail.com>
2016-11-09 12:39:40 +0100

    now running demo withGoogleMap

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,60 +1,100 @@
 import React from 'react';
-import {GoogleMapLoader, GoogleMap, Marker, Polygon} from "react-google-maps"
+import {withGoogleMap, GoogleMap, Marker} from "react-google-maps"
+
+const GettingStartedGoogleMap = withGoogleMap(props => (
+    <GoogleMap
+        ref={props.onMapLoad}
+        defaultZoom={3}
+        defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+        onClick={props.onMapClick}
+    >
+        {props.markers.map(marker => (
+            <Marker
+                {...marker}
+                onRightClick={() => props.onMarkerRightClick(marker)}
+            />
+        ))}
+    </GoogleMap>
+));
 
 export default class Map extends React.Component {
     constructor() {
         super();
         this.state = {
-            zoom: 7,
+            markers: [{
+                position: {
+                    lat: 25.0112183,
+                    lng: 121.52067570000001,
+                },
+                key: `Taiwan`,
+                defaultAnimation: 2,
+            }],
         };
-        this.handleClick = this.handleClick.bind(this);
+        this.handleMapLoad = this.handleMapLoad.bind(this);
+        this.handleMapClick = this.handleMapClick.bind(this);
+        this.handleMarkerRightClick = this.handleMarkerRightClick.bind(this);
     }
 
-    handleClick(){
-        console.log("HELLOOOOO");
+    handleMapLoad(map) {
+        this._mapComponent = map;
+        if (map) {
+            console.log(map.getZoom());
+        }
+    }
+
+    /*
+     * This is called when you click on the map.
+     * Go and try click now.
+     */
+    handleMapClick(event) {
+        const nextMarkers = [
+            ...this.state.markers,
+            {
+                position: event.latLng,
+                defaultAnimation: 2,
+                key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
+            },
+        ];
+        this.setState({
+            markers: nextMarkers,
+        });
+
+        if (nextMarkers.length === 3) {
+            this.props.toast(
+                `Right click on the marker to remove it`,
+                `Also check the code!`
+            );
+        }
+    }
+
+    handleMarkerRightClick(targetMarker) {
+        /*
+         * All you modify is data, and the view is driven by data.
+         * This is so called data-driven-development. (And yes, it's now in
+         * web front end and even with google maps API.)
+         */
+        const nextMarkers = this.state.markers.filter(marker => marker !== targetMarker);
+        this.setState({
+            markers: nextMarkers,
+        });
     }
 
     render() {
-
-        const location = {
-            lat:8.460555,
-            lng:-11.779889
-        };
-
-        const mapContainer = <div style={{height: '100%', width: '100%'}}/>
-
-        const markers = this.props.markers.map((venue, i) => {
-            const marker = {
-                position: {
-                    lat: venue.location.lat,
-                    lng: venue.location.lng
-                }
-            }
-            return <Marker key={i} {...marker}/>
-        })
-
         return (
-            <GoogleMapLoader
-                containerElement={mapContainer}
-                googleMapElement={
-                    <GoogleMap
-                        //onClick= {props.onMapClick}
-                        defaultZoom={this.state.zoom}
-                        defaultCenter={this.props.center}
-                        options={{streetViewControl: false, mapTypeControl: false}}>
-                        {markers}
-                        <Polygon
-                            //this does not work
-                            onMapClick = {this.handleClick}
-                            strokeColor='#FF0000'
-                            path = {this.props.poly}
-                            fillColor = '#FF0000'
-                            fillOpacity = '0.35'/>
-                    </GoogleMap>
-                }/>
-
-
-        )
+            <div style={{height: `100%`}}>
+                <GettingStartedGoogleMap
+                    containerElement={
+                        <div style={{ height: `100%` }} />
+                    }
+                    mapElement={
+                        <div style={{ height: `100%` }} />
+                    }
+                    onMapLoad={this.handleMapLoad}
+                    onMapClick={this.handleMapClick}
+                    markers={this.state.markers}
+                    onMarkerRightClick={this.handleMarkerRightClick}
+                />
+            </div>
+        );
     }
 }
-
