91e66194a0c5757a65768a035822b6f4a8b1f4ee
Hauglid <karltha@ifi.uio.no>
2016-11-12 22:57:09 +0100

    can now pick from suggestion list

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -4,11 +4,14 @@ import AutoComplete from 'material-ui/AutoComplete';
 import {Toolbar, ToolbarGroup, ToolbarSeparator} from 'material-ui/Toolbar';
 import {searchBy} from '../api';
 
-export default class ToolbarExamplesSimple extends React.Component {
+export default class Search extends React.Component {
 
     constructor(props) {
         super(props);
         this.onUpdateInput = this.onUpdateInput.bind(this);
+        this.onTouchTap = this.onTouchTap.bind(this);
+        this.onNewRequest = this.onNewRequest.bind(this);
+
         this.state = {
             dataSource: [],
             inputValue: '',
@@ -18,6 +21,7 @@ export default class ToolbarExamplesSimple extends React.Component {
     }
 
     onUpdateInput(inputValue) {
+        console.log("onUpdateInput");
         const self = this;
         this.setState({
             inputValue: inputValue
@@ -28,23 +32,31 @@ export default class ToolbarExamplesSimple extends React.Component {
 
     performSearch() {
         const self = this;
+        console.log("updated to: " + self.state.inputValue);
 
         if (this.state.inputValue !== '') {
             var response = searchBy("name", self.state.inputValue);
             response.then((organisationUnit) => {
-                if(organisationUnit.length > 0){
-                    var result = organisationUnit.map(function (a) {return a.displayName});
+                if (organisationUnit.length > 0) {
+                    var result = organisationUnit.map(function (a) {
+                        return a.displayName
+                    });
                     self.setState({
                         dataSource: result,
                         inputId: organisationUnit[0].id
                     });
-
                 }
-
             })
         }
     }
 
+    onTouchTap() {
+        console.log(this.state.inputValue);
+    }
+
+    onNewRequest(inputValue) {
+        this.onUpdateInput(inputValue);
+    }
 
     render() {
         return (
@@ -54,13 +66,13 @@ export default class ToolbarExamplesSimple extends React.Component {
                     dataSource={this.state.dataSource}
                     onUpdateInput={this.onUpdateInput}
                     filter={AutoComplete.caseInsensitiveFilter}
+                    onNewRequest={this.onNewRequest}
                     fullWidth={true}
                     maxSearchResults={10}
-
                 />
                 <ToolbarGroup lastChild={true}>
                     <ToolbarSeparator />
-                    <RaisedButton label="Search" primary={true}/>
+                    <RaisedButton onTouchTap={this.onTouchTap} label="Search" primary={true}/>
 
                 </ToolbarGroup>
             </Toolbar>
