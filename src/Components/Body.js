1eed766d4eae7e241725bc80bb81c2c71e811763
Hauglid <karltha@ifi.uio.no>
2016-11-14 15:39:20 +0100

    added mapinfo to body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Map from "./Map"
 import OrgUnitInfo from "./OrgUnitInfo"
 import Paper from 'material-ui/Paper';
+import MapInfo from "./MapInfo"
 
 
 export default class Body extends React.Component {
@@ -18,7 +19,7 @@ export default class Body extends React.Component {
         return (
             <div style={styleDiv.root}>
                 <Paper style={{width:"20%"}} zDepth={3} >
-
+                    <MapInfo />
                 </Paper>
                 <Paper  zDepth={3} style={{margin:"0px 5px", padding: "10px", height: 500, width: "53%"}}>
                         <Map/>
