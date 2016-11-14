1e0d82565591e57dfed348b9cc5f183ca5d7d1f8
Jostein Hellerud <jostein1993@hotmail.com>
2016-11-14 11:07:31 +0100

    removed unused constants

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -15,35 +15,6 @@ export default class Body extends React.Component {
             },
         };
 
-        const location = {
-            lat: 8.460555,
-            lng: -11.779889
-        };
-
-        const markers = [
-
-            {
-                location: {
-                    lat: 8.460555,
-                    lng: -11.779884
-                }
-            },
-            {
-                location: {
-                    lat: 8.361555,
-                    lng: -11.779882
-                }
-            }
-
-        ];
-
-        const triangleCoords = [
-            {lat: 25.774, lng: -80.190},
-            {lat: 18.466, lng: -66.118},
-            {lat: 32.321, lng: -64.757},
-            {lat: 25.774, lng: -80.190}
-        ];
-
         return (
             <div style={styleDiv.root}>
                 <Paper style={{width:"20%"}} zDepth={3} >
