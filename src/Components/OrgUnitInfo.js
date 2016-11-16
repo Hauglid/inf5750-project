3e02f74425dbc46c422e5eeead4b990bfa19385f
Krister Bakken <krister.bakken@outlook.com>
2016-11-16 16:17:18 +0100

    added save, not working yet, cancel is bugged in edit

--- src/Components/OrgUnitInfo.js
+++ src/Components/OrgUnitInfo.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {loadOrganisationUnits, loadUnitInfo, searchBy} from '../api';
-import TextFields from './TextFields';
+import {saveOrganisationUnit, loadUnitInfo} from '../api';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
@@ -8,16 +7,12 @@ export default class OrgUnitInfo extends React.Component {
     constructor(props) {
         super(props);
 
-        console.log("troll");
-
         this.state = {
             isSaving: false,
-            isLoading: false,
             unitInfo: [],
             editing: false,
-            items: [],
-            id: "",
         };
+
         this.editButton = this.editButton.bind(this);
         this.saveButton = this.saveButton.bind(this);
         this.cancelButton = this.cancelButton.bind(this);
@@ -45,6 +40,7 @@ export default class OrgUnitInfo extends React.Component {
         loadUnitInfo(id).then((organisationUnit) => {
             this.setState({
                 unitInfo: organisationUnit,
+                originalUnitInfo: organisationUnit,
             });
         });
     }
@@ -60,44 +56,42 @@ export default class OrgUnitInfo extends React.Component {
 
     newUnit() {
         console.log("New");
-        // console.log(this.state.oldUnitInfo);
         this.setState({
             editing: true,
-            oldUnitInfo: this.state.unitInfo}/*, function() {console.log(this.state.oldUnitInfo)}*/);
-        this.setState({
+            oldUnitInfo: this.state.unitInfo,
             unitInfo: {
                 displayName: "",
                 openingDate: "",
                 coordinates: "",
-                id: "",
-            }}, function() {console.log(this.state.unitInfo)
-        });
+                id: ""
+            }}, function() {console.log(this.state.unitInfo);});
+
     }
 
     editButton() {
+        console.log("Edit");
         this.setState({
             editing: true,
-            oldUnitInfo: this.state.unitInfo
+            oldUnitInfo: this.state.unitInfo,
         }, function() {
             console.log(this.state.oldUnitInfo);
         });
-        console.log("Edit");
         // this.setState({
         //     unitInfo: {
-        //         displayName: "",
-        //         openingDate: "",
-        //         coordinates: "",
-        //         id: "",
+        //         displayName: this.state.oldUnitInfo["displayName"],
+        //         openingDate: this.state.oldUnitInfo["openingDate"],
+        //         coordinates: this.state.oldUnitInfo["coordinates"],
+        //         id: this.state.oldUnitInfo["id"],
         //     }}, function() {console.log(this.state.unitInfo)
         // });
     }
 
     cancelButton() {
         console.log("Cancel");
-        console.log(this.state.oldUnitInfo);
+        // console.log(this.state.oldUnitInfo);
         this.setState({
+            unitInfo: this.state.oldUnitInfo,
             editing: false,
-            unitInfo: this.state.oldUnitInfo
         }, function() {console.log(this.state.unitInfo)});
 
     }
@@ -105,6 +99,25 @@ export default class OrgUnitInfo extends React.Component {
     saveButton() {
         console.log("Save");
         this.setState({editing: false});
+        console.log(this.isValid());
+        this.saveUnit(this.state.unitInfo);
+
+    }
+
+    saveUnit(unit) {
+        console.log(unit);
+        saveOrganisationUnit(unit)
+            .catch(() => alert("Could not save unit"));
+    }
+
+    //returns false if valid, true if not
+    isValid() {
+        return !(
+          this.state.unitInfo["displayName"] &&
+          this.state.unitInfo["openingDate"] &&
+          this.state.unitInfo["coordinates"] &&
+          this.state.unitInfo["id"]
+        );
     }
 
 
@@ -123,13 +136,33 @@ export default class OrgUnitInfo extends React.Component {
                     onClick={this.state.editing ? this.saveButton : this.newUnit}/>
                 <br/>
                 <div>
-                    <TextField disabled={!this.state.editing} onChange={this.handleChange.bind(this, "displayName")} underlineShow={this.state.editing} floatingLabelText="Name" value={this.state.unitInfo["displayName"]} />
+                    <TextField
+                        disabled={!this.state.editing}
+                        underlineShow={this.state.editing}
+                        floatingLabelText="Name"
+                        onChange={this.handleChange.bind(this, "displayName")}
+                        value={this.state.unitInfo["displayName"]} />
                     <br/>
-                    <TextField disabled={!this.state.editing} onChange={this.handleChange.bind(this, "openingDate")} underlineShow={this.state.editing} floatingLabelText="Opening date" value={this.state.unitInfo["openingDate"]} />
+                    <TextField
+                        disabled={!this.state.editing}
+                        onChange={this.handleChange.bind(this, "openingDate")}
+                        underlineShow={this.state.editing}
+                        floatingLabelText="Opening date"
+                        value={this.state.unitInfo["openingDate"]} />
                     <br/>
-                    <TextField disabled={!this.state.editing} onChange={this.handleChange.bind(this, "coordinates")} underlineShow={this.state.editing} floatingLabelText="Coordinates" value={this.state.unitInfo["coordinates"]} />
+                    <TextField
+                        disabled={!this.state.editing}
+                        onChange={this.handleChange.bind(this, "coordinates")}
+                        underlineShow={this.state.editing}
+                        floatingLabelText="Coordinates"
+                        value={this.state.unitInfo["coordinates"]} />
                     <br/>
-                    <TextField disabled={!this.state.editing} onChange={this.handleChange.bind(this, "id")} underlineShow={this.state.editing} floatingLabelText="ID" value={this.state.unitInfo["id"]} />
+                    <TextField
+                        disabled={!this.state.editing}
+                        onChange={this.handleChange.bind(this, "id")}
+                        underlineShow={this.state.editing}
+                        floatingLabelText="ID"
+                        value={this.state.unitInfo["id"]} />
                     <br/>
 
 
