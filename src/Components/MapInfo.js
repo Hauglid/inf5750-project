09d64634595e6f5a3e6833281439e4671c11fb2a
Hauglid <karltha@ifi.uio.no>
2016-11-20 15:19:14 +0100

    added parent name to list

--- src/Components/MapInfo.js
+++ src/Components/MapInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {searchBy} from '../api';
+import {searchBy, loadUnitInfo} from '../api';
 import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 
 export default class MapInfo extends React.Component {
 
@@ -10,6 +11,8 @@ export default class MapInfo extends React.Component {
         this.state = {
             unitInfo: [],
             id: props.id,
+            nameParent: "",
+
         };
         this.load = this.load.bind(this);
     }
@@ -17,11 +20,12 @@ export default class MapInfo extends React.Component {
     componentDidMount() {
         this.load();
     }
-    componentWillReceiveProps(nextProps){
-        if(nextProps.id != this.state.id){
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.id != this.state.id) {
             this.setState({
                 id: nextProps.id,
-            }, function (){
+            }, function () {
                 this.load();
             });
         }
@@ -30,6 +34,15 @@ export default class MapInfo extends React.Component {
     load() {
 
         const response = searchBy("parent.id", this.state.id);
+
+        loadUnitInfo(this.state.id)
+            .then(({name}) => name)
+            .then((name) => {
+                this.setState({
+                    name: name,
+                });
+            });
+
         response.then((unit) => {
             var result = unit.map(function (a) {
                 return {
@@ -48,19 +61,23 @@ export default class MapInfo extends React.Component {
     render() {
 
         return (
-            <List style={{height: 500, overflowY: "scroll"}}>
-                {this.state.unitInfo.map(function (item) {
-                    return (
-                        <ListItem
-                            key={item.id}
-                            value={item.id}
-                            primaryText={item.name}
-                        />
-                    )
+            <div style={{height: "500"}}>
+                <h2>{this.state.name}</h2>
+                <Divider/>
+                <List style={{height: "435" , overflowY: "scroll"}}>
+                    {this.state.unitInfo.map(function (item) {
+                        return (
+                            <ListItem
+                                key={item.id}
+                                value={item.id}
+                                primaryText={item.name}
+                            />
+                        )
 
-                })}
+                    })}
 
-            </List>
+                </List>
+            </div>
         );
     }
 }
