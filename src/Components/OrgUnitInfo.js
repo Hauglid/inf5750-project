97f394545c2dd0b8239557ce343cba20ad7fdd6b
Krister Bakken <krister.bakken@outlok.com>
2016-11-29 16:26:45 +0100

    fixed warning, addec documentation to api.js

--- src/Components/OrgUnitInfo.js
+++ src/Components/OrgUnitInfo.js
@@ -12,6 +12,7 @@ export default class OrgUnitInfo extends React.Component {
                 displayName: "",
                 openingDate: ""
             },
+            districtName: "",
             editing: false,
         };
 
--- src/api.js
+++ src/api.js
@@ -1,4 +1,8 @@
 /**
+ * This file is heavily inspired (read: copied and edited) by the api.js file in the
+ * DHIS2 organisation-unit-app-inf5750 project (https://github.com/dhis2/organisation-unit-app-inf5750)
+ */
+/**
  * `serverUrl` contains the api location of the server. You would generally get the baseUrl from the manifest.webapp
  * as described here http://dhis2.github.io/dhis2-docs/master/en/developer/html/apps_creating_apps.html
  *
@@ -7,6 +11,7 @@
  */
 // const serverUrl = 'http://localhost:8080/dhis/api';
 const serverUrl = 'https://play.dhis2.org/dev/api';
+//const serverUrl = 'https://play.dhis2.org/test/api'; // we are getting some weird behaviour when using this, so we're sticking to dev
 const basicAuth = `Basic ${btoa('admin:district')}`;
 
 /**
@@ -31,6 +36,7 @@ function onlySuccessResponses(response) {
     return Promise.reject(response);
 }
 
+//not used, but left here in case of future need
 export function loadOrganisationUnits(level) {
     // Load the organisation units but only the first level and the do not use paging
     return fetch(`${serverUrl}/organisationUnits?paging=false&level=${level}`, fetchOptions)
@@ -40,6 +46,7 @@ export function loadOrganisationUnits(level) {
         .then(({organisationUnits}) => organisationUnits);
 }
 
+//not used, but left here in case of future need
 export function loadAllUnits() {
     console.log("api.loadAllUnits");
     return fetch(`${serverUrl}/organisationUnits`, fetchOptions)
@@ -49,32 +56,50 @@ export function loadAllUnits() {
 
 }
 
+/**
+ * Fetches information about unit with given id
+ * @param unitId
+ * @returns {Promise.<TResult>} response from server
+ */
 export function loadUnitInfo(unitId) {
     return fetch(`${serverUrl}/organisationUnits/${unitId}`, fetchOptions)
         .then(onlySuccessResponses)
         .then(response => response.json());
 }
 
+/**
+ * Fetches information from the server, based on given filter and value.
+ * @param filter what to filter base on
+ * @param value "search term"
+ * @returns {Promise.<TResult>} response from server
+ */
 export function searchBy(filter, value) {
-    console.log("api.searchBy");
     return fetch(`${serverUrl}/organisationUnits/?paging=false&filter=${filter}:^ilike:${value}`, fetchOptions)
         .then(onlySuccessResponses)
         .then(response => response.json())
         .then(({organisationUnits}) => organisationUnits);
 }
 
+//not used, but left here in case of future need
 export function loadUnitInfoLvl(level) {
     return fetch(`${serverUrl}/organisationUnits?level=${level}&paging=false`, fetchOptions)
         .then(onlySuccessResponses)
         .then(response => response.json());
 }
-//example of fields: fields=id,name,level
+
+//not used, but left here in case of future need
 export function loadUnitInfoLvlAndfields(level, fields) {
+    //example of fields: fields=id,name,level
     return fetch(`${serverUrl}/organisationUnits?level=${level}&paging=false&fields=${fields}`, fetchOptions)
         .then(onlySuccessResponses)
         .then(response => response.json());
 }
 
+/**
+ * Saves the given unit to the DHIS2 server
+ * @param organisationUnit unit to be saved
+ * @returns {Promise.<TResult>} response from server
+ */
 export function saveOrganisationUnit(organisationUnit) {
     // POST the payload to the server to save the organisationUnit
     return fetch(`${serverUrl}/organisationUnits`, Object.assign({}, fetchOptions, {
@@ -84,10 +109,14 @@ export function saveOrganisationUnit(organisationUnit) {
         .then(onlySuccessResponses)
         // Parse the json response
         .then(response => response.json())
-        // Log any errors to the console. (Should probably do some better error handling);
         .catch(error => console.error(error));
 }
 
+/**
+ * Updates the unit with new values
+ * @param organisationUnit unit to be changed, with new values
+ * @returns {Promise.<TResult>} response from server
+ */
 export function updateOrganisationUnit(organisationUnit) {
     // POST the payload to the server to save the organisationUnit
     return fetch(`${serverUrl}/organisationUnits/${organisationUnit.id}`, Object.assign({}, fetchOptions, {
