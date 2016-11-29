5a782d62cc173d75cf9ed048ef560b1a3816c7b7
skilbert <jostein1993@hotmail.com>
2016-11-29 16:04:21 +0100

    reformated code

--- src/Components/Toolbox.js
+++ src/Components/Toolbox.js
@@ -21,7 +21,7 @@ export function getDistance(p1, p2) {
  * @param x: number to be converted
  * @returns {number}: the converted result
  */
-function rad (x) {
+function rad(x) {
     return x * Math.PI / 180;
 }
 
@@ -30,30 +30,30 @@ function rad (x) {
  * @param polygon: polygon to find the center of
  * @returns {{lat: number, lng: number}}: center as a latLng object
  */
-export function findCenter(polygon){
+export function findCenter(polygon) {
     var x1 = 1000.0;
     var x2 = 0.0;
     var y1 = 0.0;
     var y2 = -1000.0;
 
-    for(var i = 0; i < polygon.length; i++){
-        if(polygon[i].lat < x1){
+    for (var i = 0; i < polygon.length; i++) {
+        if (polygon[i].lat < x1) {
             x1 = polygon[i].lat;
         }
-        if(polygon[i].lat > x2){
+        if (polygon[i].lat > x2) {
             x2 = polygon[i].lat;
         }
-        if(polygon[i].lng < y1){
+        if (polygon[i].lng < y1) {
             y1 = polygon[i].lng;
         }
-        if(polygon[i].lng > y2){
+        if (polygon[i].lng > y2) {
             y2 = polygon[i].lng;
         }
     }
 
     const center = {
-        lat: x1+((x2-x1)/2),
-        lng: y1+((y2-y1)/2),
+        lat: x1 + ((x2 - x1) / 2),
+        lng: y1 + ((y2 - y1) / 2),
     }
     return center;
 }
